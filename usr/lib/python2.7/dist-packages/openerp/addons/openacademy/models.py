3418b46004f1c066dc32dda81ecdc47ab45dbb14
nelson <npedrosapena@danielcastelao.org>
2015-03-27 11:52:38 +0100

    commit inicial, ya muestra el hola mundo

---
+++ usr/lib/python2.7/dist-packages/openerp/addons/openacademy/models.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+# class openacademy(models.Model):
+#     _name = 'openacademy.openacademy'
+
+#     name = fields.Char()
+
