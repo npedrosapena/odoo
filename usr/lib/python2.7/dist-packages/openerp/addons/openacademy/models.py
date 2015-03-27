2ed264b544ce9e7de536fc320b91e6d2234910b2
nelson <npedrosapena@danielcastelao.org>
2015-03-27 12:24:02 +0100

    definida una clase con los atributos minimos

--- usr/lib/python2.7/dist-packages/openerp/addons/openacademy/models.py
+++ usr/lib/python2.7/dist-packages/openerp/addons/openacademy/models.py
@@ -3,6 +3,17 @@
 
 from openerp import models, fields, api
 
+class Course(models.Model):
+    _name = 'openacademy.course'
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+
+
+
+
+
+
 # class openacademy(models.Model):
 #     _name = 'openacademy.openacademy'
 
