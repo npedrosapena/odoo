052037fb2337bd7a8b6f7a4f8eaa085411d04ff1
nelson <npedrosapena@danielcastelao.org>
2015-04-05 12:17:01 +0200

    cambiamos el numero de version

--- usr/lib/python2.7/dist-packages/openerp/addons/openacademy/__openerp__.py
+++ usr/lib/python2.7/dist-packages/openerp/addons/openacademy/__openerp__.py
@@ -21,7 +21,7 @@
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
     'category': 'Test',
-    'version': '0.1',
+    'version': '1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
