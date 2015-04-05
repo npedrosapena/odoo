c710f061717e6d3680f1fd6d057e63c943c85f31
nelson <npedrosapena@danielcastelao.org>
2015-04-05 12:18:16 +0200

    session model paso 1

--- usr/lib/python2.7/dist-packages/openerp/addons/openacademy/models.py
+++ usr/lib/python2.7/dist-packages/openerp/addons/openacademy/models.py
@@ -8,6 +8,10 @@ class Course(models.Model):
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
+    
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
 
 
 
